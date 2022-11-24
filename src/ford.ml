658d5226ce3791c56154db3ecccca7a3e5cd9931
Raveau Matthis <raveau@insa-toulouse.fr>
2022-11-24 10:45:29 +0100

    début Ford

---
+++ src/ford.ml
@@ -0,0 +1,6 @@
+open Graph
+open Gfile
+open Tools
+
+let initalize_flow gr = 
+  gmap gr (fun x -> 0) 
---
+++ src/ford.mli
@@ -0,0 +1,7 @@
+open Graph
+open Gfile
+open Tools
+
+val initialize_flow: int graph -> int graph
+
+(* Graph résiduel à faire : le graph avec les arcs dans les deux sens 0/5 etc)
